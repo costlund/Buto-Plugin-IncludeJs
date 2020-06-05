e1ce9aabf418fe2667fb590aca43569d17cb403b
Claes Östlund <claes@stenbergit.se>
2020-06-05 12:21:12 +0200

    1.1.0

--- PluginIncludeJs.php
+++ PluginIncludeJs.php
@@ -14,15 +14,31 @@ class PluginIncludeJs{
     /**
      * 
      */
-    if(!wfFilesystem::fileExist(wfGlobals::getWebDir().$data->get('data/src'))){
-      throw new Exception(__CLASS__.' says: File '.wfGlobals::getWebDir().$data->get('data/src').' does not exist!');
-    }
-    /**
-     * 
-     */
-    $filetime = wfFilesystem::getFiletime(wfGlobals::getWebDir().$data->get('data/src'));
     $element = array();
-    $element[] = wfDocument::createHtmlElement('script', null, array('src' => $data->get('data/src').'?t='.$filetime, 'type' => 'text/javascript'));
+    if(!is_array($data->get('data/src'))){
+      /**
+       * 
+       */
+      if(!wfFilesystem::fileExist(wfGlobals::getWebDir().$data->get('data/src'))){
+        throw new Exception(__CLASS__.' says: File '.wfGlobals::getWebDir().$data->get('data/src').' does not exist!');
+      }
+      /**
+       * 
+       */
+      $filetime = wfFilesystem::getFiletime(wfGlobals::getWebDir().$data->get('data/src'));
+      $element[] = wfDocument::createHtmlElement('script', null, array('src' => $data->get('data/src').'?t='.$filetime, 'type' => 'text/javascript'));
+    }else{
+      foreach ($data->get('data/src') as $value) {
+        /**
+         * 
+         */
+        if(!wfFilesystem::fileExist(wfGlobals::getWebDir().$value)){
+          throw new Exception(__CLASS__.' says: File '.wfGlobals::getWebDir().$value.' does not exist!');
+        }
+        $filetime = wfFilesystem::getFiletime(wfGlobals::getWebDir().$value);
+        $element[] = wfDocument::createHtmlElement('script', null, array('src' => $value.'?t='.$filetime, 'type' => 'text/javascript'));
+      }
+    }
     wfDocument::renderElement($element);
   }
 }
--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Include script element with time param to force browser to download latest version. The file must exist in web root folder.
 
-Via PHP.
+##PHP
 ```
 wfPlugin::enable('include/js');
 wfDocument::createWidget('include/js', 'include', array('src' => '/plugin/_folder_one_/_folder_two_/function.js'));
 ```
 
-Via YML.
+##YML
 ```
 type: widget
 data:
@@ -16,4 +16,17 @@ data:
   method: include
   data:
     src: '/plugin/_folder_one_/_folder_two_/function.js'
+```
+
+##Param src as array
+One could set param src as array to include multiple files.
+```
+type: widget
+data:
+  plugin: 'include/js'
+  method: include
+  data:
+    src:
+      - '/plugin/_folder_one_/_folder_two_/function.js'
+      - '/plugin/_folder_one_/_folder_two_/function2.js'
 ```
